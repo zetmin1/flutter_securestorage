f5ef8ff0892cd69fceae3b4d224713a76d5a4af9
zetmin <lzetminl@gmail.com>
2023-02-22 20:33:42 +0900

    로그인/로그아웃 인터페이스 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart'; // DIO 패키지로 HTTP 통신
+import 'dart:convert'; // JSON Encode, Decode를 위한 패키지
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // flutter_secure_storage 패키지
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Secure Storage',
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  var username = TextEditingController(); // id 입력 저장
+  var password = TextEditingController(); // pw 입력 저장
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  static final storage =
+      FlutterSecureStorage(); // FlutterSecureStorage를 storage로 저장
+  dynamic userInfo = ''; // storage에 있는 유저 정보를 저장
+
+  //flutter_secure_storage 사용을 위한 초기화 작업
+  @override
+  void initState() {
+    super.initState();
+
+    // 비동기로 flutter secure storage 정보를 불러오는 작업
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _asyncMethod();
     });
+  }
+
+  _asyncMethod() async {
+    // read 함수로 key값에 맞는 정보를 불러오고 데이터타입은 String 타입
+    // 데이터가 없을때는 null을 반환
+    userInfo = await storage.read(key: 'login');
+
+    // user의 정보가 있다면 로그인 후 들어가는 첫 페이지로 넘어가게 합니다.
+    if (userInfo != null) {
+      Navigator.pushNamed(context, '/main');
+    } else {
+      print('로그인이 필요합니다');
+    }
+  }
+
+  // 로그인 버튼 누르면 실행
+  loginAction(accountName, password) async {
+    try {
+      var dio = Dio();
+      var param = {'account_name': '$accountName', 'password': '$password'};
+
+      Response response = await dio.post('로그인 API URL', data: param);
+
+      if (response.statusCode == 200) {
+        final jsonBody = json.decode(response.data['user_id'].toString());
+        // 직렬화를 이용하여 데이터를 입출력하기 위해 model.dart에 Login 정의 참고
+        var val = jsonEncode(Login('$accountName', '$password', '$jsonBody'));
+
+        await storage.write(
+          key: 'login',
+          value: val,
+        );
+        print('접속 성공!');
+        return true;
+      } else {
+        print('error');
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+          child: ListView(
+        children: [
+          // 아이디 입력 영역
+          TextField(
+            controller: username,
+            decoration: InputDecoration(
+              labelText: 'Username',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          // 비밀번호 입력 영역
+          TextField(
+            controller: password,
+            decoration: InputDecoration(
+              labelText: 'Password',
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          // 로그인 버튼
+          ElevatedButton(
+            onPressed: () async {
+              if (await loginAction(username.text, password.text) == true) {
+                print('로그인 성공');
+                Navigator.pushNamed(context, '/service'); // 로그인 이후 서비스 화면으로 이동
+              } else {
+                print('로그인 실패');
+              }
+            },
+            child: Text('로그인 하기'),
+          ),
+        ],
+      )),
     );
   }
+}
+
+class Login {
+  final String accountName;
+  final String password;
+  final String user_id;
+
+  Login(this.accountName, this.password, this.user_id);
+
+  Login.fromJson(Map<String, dynamic> json)
+      : accountName = json['accountName'],
+        password = json['password'],
+        user_id = json['user_id'];
+
+  Map<String, dynamic> toJson() => {
+        'accountName': accountName,
+        'password': password,
+        'user_id': user_id,
+      };
 }
